f11cf5e828346a61ae068bf631af7f19b36ea84e
MaxTGH <43712547+MaxTGH@users.noreply.github.com>
2026-07-05 21:09:37 -0700

    Update config.m

--- config.m
+++ config.m
@@ -1,5 +1,6 @@
 % config: contains project-specific parameters that may be swapped out
 
-REMORA.lt.lVis_det.labels = {'A', 'B', 'D', '20Hz', '40Hz'};            % Call types
-REMORA.lt.lVis_det.wmdFolder = 'C:/path/to/WhaleMoanDetector';          % Where your WhaleMoanDetector folder is
-REMORA.lt.lVis_det.pyenvFolder = 'C:/path/to/your/python/environment';  % Where your whalemoandetector python environment is
+% REMORA.lt.lVis_det.labels = {'A', 'B', 'D', '20Hz', '40Hz'};            % Call types
+REMORA.lt.lVis_det.labels = {'NARW_up-call', 'Bm_A_North_Atlantic', 'Bm_B_North_Atlantic', 'Bm_AB_North_Atlantic', 'Bm_arch','Ba_pulse-call', 'Bp_20Hz', 'Bp_40Hz', 'Bb_down-sweep'};            % Call types
+REMORA.lt.lVis_det.wmdFolder = 'D:/Tools/WhaleMoanDetector-main';          % Where your WhaleMoanDetector folder is
+REMORA.lt.lVis_det.pyenvFolder = 'C:/Users/HARP/.conda/envs/whalemoandetector';  % Where your whalemoandetector python environment is
